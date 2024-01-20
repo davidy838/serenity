8f3863076b7cb60eb9c8246104b525de5d8bffcd
stephanieccchow <stephanieccchow@gmail.com>
2024-01-20 13:48:36 -0800

    header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
 
         <Routes>
           <Route exact path ='/' element={<Home />} />
