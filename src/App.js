8df0877ee023e2498e43641cc432af7b9ed7e214
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-01-20 12:44:35 -0800

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <h1>Serenity</h1>
+  )
+}
 
 function App() {
   return (
