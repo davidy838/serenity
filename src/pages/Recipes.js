f09c7afd66cbb248ab54a60b210a753a69a3dfec
davidy838 <davidyang838@icloud.com>
2024-01-20 16:38:01 -0800

    header and recipes

---
+++ src/pages/Recipes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RecipesSection from '../components/recipes-section/RecipesSection';
+
+function Reviews() {
+  return (
+    <RecipesSection />
+  );
+}
+
+export default Reviews;
