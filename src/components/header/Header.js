f05257751dff80226a5e6f626d5122b4a71e868c
davidy838 <davidyang838@icloud.com>
2024-01-20 17:14:57 -0800

    header

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 className="h1">Serenity</h1>
+        </header>
+    );
+
+
+
+
+}
+export default Header;
