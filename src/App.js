caf917e3b204b8c3a4063c81ca1509c965f3a177
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-01-20 20:50:36 -0800

    fix bug

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Routes>
           <Route exact path ='/' element={<Home />} />
 
-          <Route exact path ='/breathing' element={<Breathing />} 
+          <Route exact path ='/breathing' element={<Breathing />}  />
           <Route exact path ='/recipes' element={<Recipes />} />
 
 
