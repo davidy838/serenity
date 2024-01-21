9e2a217e7a34b11c747c834e6e627930c92d3051
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-01-20 22:21:58 -0800

    edited text

--- src/pages/End.js
+++ src/pages/End.js
@@ -5,8 +5,9 @@ import {Link} from 'react-router-dom';
 function End() {
     return (
       <div className="center-container"> {/* Apply the center-container class */}
-        <h1>Thank you for destressing with Serenity</h1>
-        <p>Click the button to restart!</p>
+        <h1>Wahoo! That’s all for now. Great job!</h1>
+        <p>Thank you for taking the time to take care of yourself. Your body and mind appreciate it and we hope that you feel more serene.</p>
+        <p>You are always welcome to come back to this tool when you are needing, or even using this as a preventative measure.</p>
   
         {/* Central Start Button as a Link */}
         <Link to="/">
