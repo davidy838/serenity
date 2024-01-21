1c399a0f8e86e4728d77f7988f4ee9fd5c121c01
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-01-20 18:03:22 -0800

    circle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react';
 import Home from './pages/Home';
+import Breathing from './pages/Breathing';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
 
         <Routes>
           <Route exact path ='/' element={<Home />} />
+          <Route exact path ='/breathing' element={<Breathing />} />
 
 
 
