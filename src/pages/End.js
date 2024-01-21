591b6ebd8ef8dd0592c612e84e9ee2ba7df69b92
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-01-20 22:32:47 -0800

    images

--- src/pages/End.js
+++ src/pages/End.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 function End() {
     return (
       <div className="center-container"> {/* Apply the center-container class */}
-        <h1>Wahoo! That’s all for now. Great job!</h1>
+        <h1>Wahoo! That's all for now. Great job!</h1>
         <p>Thank you for taking the time to take care of yourself. Your body and mind appreciate it and we hope that you feel more serene.</p>
         <p>You are always welcome to come back to this tool when you are needing, or even using this as a preventative measure.</p>
   
